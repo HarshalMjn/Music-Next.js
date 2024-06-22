ad2731cf31bb4eb415bd1f1a4d46c1a7ce58b75f
HarshalMjn <mahajanh006@gmail.com>
2024-06-22 22:48:07 +0530

    config plugins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased
    bg-grid-white/[0.02]">
-    <h1 className="text-2xl text-center text-white"></h1>
+    <HeroSection/>
+    <FeaturedCourses/>
+    <WhyChooseUs/>
    </main>
   );
 }
